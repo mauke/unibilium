f2166bbcb16dd85f21871e83f87c5e3fe360f990
Lukas Mai <l.mai@web.de>
2015-08-30 21:08:56 +0200

    gcc 5 no longer warns for {0}

--- maint.mk
+++ maint.mk
@@ -1,2 +1,2 @@
-CFLAGS=-std=gnu99 -pedantic -Wall -Wextra -Wno-missing-field-initializers -Wundef -Wshadow -Wbad-function-cast -Wcast-align -Wwrite-strings -Wstrict-prototypes -Wmissing-prototypes -Wnested-externs -Winline -Wdisabled-optimization -O2 -pipe -fstack-protector-all -fsanitize=address -fsanitize=undefined
+CFLAGS=-std=gnu99 -pedantic -Wall -Wextra -Wundef -Wshadow -Wbad-function-cast -Wcast-align -Wwrite-strings -Wstrict-prototypes -Wmissing-prototypes -Wnested-externs -Winline -Wdisabled-optimization -O2 -pipe -fstack-protector-all -fsanitize=address -fsanitize=undefined
 LDFLAGS=-fsanitize=address -fsanitize=undefined
